62519e111316078d75af0f5d7a0120628a30684b
Jason Desmond <jdesmond79@gmail.com>
2025-10-15 11:34:42 -0400

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,
+    globals: true, // IMPORTANT: enables test, expect, etc. globally
     environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-  },
+    setupFiles: './src/test/setup.ts' // ensure this path is correct
+  }
 });
