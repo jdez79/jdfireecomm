26c2c9730bba6c83749aae4af6b404db818bf62f
Jason Desmond <jdesmond79@gmail.com>
2025-10-15 13:34:47 -0400

    home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import type { Product } from "../../types/types";
 import ProductCard from '../../components/ProductCard';
 import { Container, Row, Col, Form, Button, Spinner } from 'react-bootstrap';
-import { collection, getDocs } from "Firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from '../../lib/Firebase/firebase';
 
 const Home: React.FC = () => {
